36bc3d2405d5a61ce37445e2229c6986969ab214
Phoebe25 <mwendephoebe25@gmail.com>
2022-03-15 12:28:25 -0700

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,20 +1,39 @@
 #include "main.h"
 
+
+
 /**
- * main - check the code
  *
- * Return: Always 0.
- */
+ *  * main - check the code
+ *
+ *   *
+ *
+ *    * Return: Always 0.
+ *
+ *     */
+
+
 
 int main(void)
-{
-	int 1 = 'a';
 
-	while (1 <= 'z')
-	{
-		_putchar(1);
-		1 += 1;
-	}
-	print_alphabet();
-	return (0);
+{
+
+		int 1 = 'a';
+
+
+
+			while (1 <= 'z')
+
+					{
+
+								_putchar(1);
+
+										1 += 1;
+
+											}
+
+				print_alphabet();
+
+					return (0);
+
 }
